596515896d9aa645f3cf3a79665a4eab7f1a2513
Twolar <t4ybennett@gmail.com>
2023-02-14 09:56:22 +1300

    Remove old code and add functionality for new completed run POST request

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-// const yargs = require('yargs');
-// const weather = require('./src/weather');
 const {logger} = require('./src/utility/logger');
-// const completedRunsFile = require('./src/file');s
 
 const app = require('./app');
 
@@ -9,66 +6,3 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
   logger.info(`Listening: http://localhost:${port}`);
 });
-
-// // Customize yargs version
-// yargs.version('1.1.0');
-
-// async function generateOutputString(runCompleted, date) {
-//     logger.info("Generating output string");
-//     var weatherForSingleDay = await weather.fetchWeatherForSingleDate(date);
-//     var outputString = formatRunDetailsIntoString(runCompleted) + " | " + weather.formatWeatherIntoString(weatherForSingleDay);
-//     return outputString;
-// }
-
-// function formatRunDetailsIntoString(runCompleted) {
-//     logger.info("Formatting run details into string");
-//     var runDetailsString = `Date: ${runCompleted.dateCompleted} | Distance Run: ${runCompleted.distanceRan}km | Time it took: ${runCompleted.timeTaken} minutes | Average Pace: ${runCompleted.averagePace} min/km`;
-//     return runDetailsString
-// }
-   
-// // Create add command
-// yargs.command({
-//     command: 'runCompleted',
-//     describe: 'Calculates stats for a run completed based off time and distance',
-//     builder: {
-//         dateCompleted: {
-//             describe: 'Date of run completed in format YYYY-MM-DD',
-//             demandOption: true,  // Required
-//             type: 'string'     
-//         },
-//         distanceCompleted: {  
-//             describe: 'Distance ran',
-//             demandOption: true,
-//             type: 'number'
-//         },
-//         timeTaken: {  
-//             describe: 'How long the run took in minutes',
-//             demandOption: true,
-//             type: 'number'
-//         },
-//     },
-  
-//     // Function for your command
-//     handler(argv) {
-//         const runCompleted = {
-//             distanceRan: argv.distanceCompleted,
-//             timeTaken: argv.timeTaken,
-//             averagePace: Number(argv.timeTaken/argv.distanceCompleted).toFixed(2),
-//             dateCompleted: argv.dateCompleted
-//         };
-
-//         generateOutputString(runCompleted, argv.dateCompleted)
-//         .then(outputString =>  {
-//         console.log("New run added:\n" + outputString);
-//         completedRunsFile.addCompletedRun(outputString);
-//         })
-//         .catch(error => logger.error(error));
-
-//         let completedRunsArray = completedRunsFile.getCompletedRuns();
-//         let completedRunsCount = completedRunsArray.filter(run => run).length;
-
-//         console.log(`${completedRunsCount} Completed runs in file: ${completedRunsArray.map( x => '\n' + x)}`);
-//     }
-// })
-
-// yargs.parse() // To set above changes
--- src/api/completedRuns.js
+++ src/api/completedRuns.js
@@ -1,18 +1,55 @@
 const express = require('express');
+const completedRunsFile = require('../file');
+const {logger} = require('../utility/logger');
+const weather = require('../weather');
 
 const router = express.Router();
 
 // Get all completed runs
 router.get('/', (req, res) => {
-  res.json(['😀', '😳', '🙄']);
+    let completedRunsArray = completedRunsFile.getCompletedRuns();
+    res.json(completedRunsArray);
 });
 
 // Get single completed run based on ID
 
 // Add new completed run
+router.post('/addNew', (req, res) => {
+    const runCompleted = {
+        dateCompleted: req.body.dateCompleted,
+        distanceRan: req.body.distanceRan,
+        timeTaken: req.body.timeTaken,
+    };
+    
+    generateOutputString(runCompleted)
+    .then(outputString =>  {
+        console.log("New run added:\n" + outputString);
+        completedRunsFile.addCompletedRun(outputString);
+    })
+    .catch(error => logger.error(error));
+
+    res.send('POST request to add new completed run');
+});
 
 // Update existing completed run based on ID
 
 // Delete existing completed run based on ID
+
+//#region Other Methods
+
+function formatRunDetailsIntoString(runCompleted) {
+    logger.info("Formatting run details into string");
+    var runDetailsString = `Date: ${runCompleted.dateCompleted} | Distance Run: ${runCompleted.distanceRan}km | Time it took: ${runCompleted.timeTaken} minutes | Average Pace: ${Number(runCompleted.timeTaken/runCompleted.distanceRan).toFixed(2)} min/km`;
+    return runDetailsString
+}
+
+async function generateOutputString(runCompleted) {
+    logger.info("Generating output string");
+    var weatherForSingleDay = await weather.fetchWeatherForSingleDate(runCompleted.dateCompleted);
+    var outputString = formatRunDetailsIntoString(runCompleted) + " | " + weather.formatWeatherIntoString(weatherForSingleDay);
+    return outputString;
+}
+
+//#endregion
 
 module.exports = router;
