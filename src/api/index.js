d8687834f29a81a6fb2c039f712168025ed65820
Twolar <t4ybennett@gmail.com>
2023-02-16 10:19:05 +1300

    Adding swagger init

--- src/api/index.js
+++ src/api/index.js
@@ -4,6 +4,12 @@ const completedRuns = require('./completedRuns');
 
 const router = express.Router();
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../../swagger.json');
+
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
 router.get('/', (req, res) => {
   res.json({
     message: 'API - 👋🌎🌍🌏',
