c6c2ae283f47b1fe977d722a2b4091a83dd0529e
Twolar <t4ybennett@gmail.com>
2023-02-22 07:27:53 +1300

    refactor based index + app js

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const api = require('./src/api');
-
-const app = express();
-
-app.use(express.json());
-app.use('/api/v1', api);
-
-module.exports = app;
--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const {logger} = require('./src/utility/logger');
+const express = require('express');
+const api = require('./src/api');
 
-const app = require('./app');
+const cors = require('cors');
+const middleware = require("./middleware");
+
+const app = express();
 
 const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use('/api/v1', api);
+app.use(
+  express.urlencoded(),
+  cors({
+      origin: `http://localhost:${port}`
+  })
+);
+
 app.listen(port, () => {
   logger.info(`Listening: http://localhost:${port}`);
 });
+
+module.exports = app;
